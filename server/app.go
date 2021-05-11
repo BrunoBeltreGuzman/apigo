5de4841c7bfb23a45949f3a77640c1bd0421895a
@BrunoDev <68261773+BrunoBeltreGuzman@users.noreply.github.com>
2021-05-11 17:26:36 -0400

    init commit 😀
    init commit 😀

---
+++ server/app.go
@@ -0,0 +1,25 @@
+package server
+
+import (
+	"apigo/routers"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func App() {
+	myrouters := routers.GetRouters()
+
+	server := &http.Server{
+		Addr:           ":3000",
+		Handler:        myrouters,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	fmt.Println("Server starting successfully")
+	fmt.Println("http://localhost:3000")
+	log.Fatal(server.ListenAndServe())
+
+}
